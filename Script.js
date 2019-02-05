8aa5b3c32c18c41cfcd9ed383f446d4bda9a43d5
Rasshole <47320321+Rasshole@users.noreply.github.com>
2019-02-05 14:17:26 +0100

    patch v0.3
    fixed a lot of code

--- Script.js
+++ Script.js
@@ -1,11 +1,8 @@
 
 function insertText() {
-	document.getElementById("newContent").innerHTML = "Fitness Digital Monitor. helps you keep track of your fitness routines: How much weight you use, how many reps you take, how big your reps are and how long you take to complete your sets! This will help you keep track of your development as well as how much weight you used on previous workouts on the different machines. It is also great at motivating you, seeing your constant improvements";		
-	
-}
-function linkStart() {
-	href="Measurements.html"
-}
+	document.getElementById("aboutContent").innerHTML = "Fitness Digital Monitor. helps you keep track of your fitness routines: How much weight you use, how many reps you take, how big your reps are and how long you take to complete your sets! This will help you keep track of your development as well as how much weight you used on previous workouts on the different machines. It is also great at motivating you, seeing your constant improvements";		
+	}
+
 function goToSite() {
 	windows.location = "Measurements.HTML";
 }
